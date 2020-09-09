0eac7aee073cc7541e1a2ac1c9d0fee9f743419b
harshwardhan <iharshwardhanchaudhary@gmail.com>
2020-09-09 23:20:24 +0530

    Updates

---
+++ precache-manifest.bd4ee84cf47409018e08edc3155a2ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07429afe447a6cae7008620da89a16a8",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "34e948184725f0aa7a62",
+    "url": "/react-todo/static/css/main.8ec9664e.chunk.css"
+  },
+  {
+    "revision": "64e66d747ce4fef0513e",
+    "url": "/react-todo/static/js/2.6deef6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.6deef6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e948184725f0aa7a62",
+    "url": "/react-todo/static/js/main.0ad65a47.chunk.js"
+  },
+  {
+    "revision": "ce701484895442b2eb85",
+    "url": "/react-todo/static/js/runtime-main.e4e550a5.js"
+  }
+]);
